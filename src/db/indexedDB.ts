7f4cd33485e14f13f42d98b55e8396930afc3fee
Felipe U <felipe_uv@outlook.com>
2025-11-07 14:19:41 -0500

    feat: Implement DB (IndexedDB) for habits - init, add and fetch habits operations were implemented

---
+++ src/db/indexedDB.ts
@@ -0,0 +1,57 @@
+import type { Habit } from '../types'
+
+const DB_NAME = 'ForgeDB'
+const DB_VERSION = 1
+const STORE_NAME = 'habits'
+
+let db: IDBDatabase | null = null
+
+export function initDB(): Promise<IDBDatabase> {
+  return new Promise((resolve, reject) => {
+    if (db) return resolve(db)
+
+    const request = indexedDB.open(DB_NAME, DB_VERSION)
+
+    request.onupgradeneeded = () => {
+      const db = request.result
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true })
+      }
+    }
+
+    request.onsuccess = () => {
+      db = request.result
+      resolve(db)
+    }
+
+    request.onerror = () => {
+      reject(request.error)
+    }
+  })
+}
+
+export async function addHabit(habit: Omit<Habit, 'id'>) {
+  const database = await initDB()
+
+  return new Promise((resolve, reject) => {
+    const tx = database.transaction(STORE_NAME, 'readwrite')
+    const store = tx.objectStore(STORE_NAME)
+    const request = store.add(habit)
+
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
+}
+
+export async function getHabits() {
+  const database = await initDB()
+
+  return new Promise((resolve, reject) => {
+    const tx = database.transaction(STORE_NAME, 'readonly')
+    const store = tx.objectStore(STORE_NAME)
+    const request = store.getAll()
+
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,9 @@
 export type User = {
   name: string
 }
+
+export type Habit = {
+  id?: number
+  name: string
+  createdAt: Date
+}

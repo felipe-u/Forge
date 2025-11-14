3e3877c52b9f4c9da4574cd07e7613528d40a2c1
Felipe U <felipe_uv@outlook.com>
2025-11-14 15:48:11 -0500

    feat: Implement Lazy Loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,29 +1,34 @@
+import { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router'
-import Dashboard from '../pages/private/Dashboard'
-import Habits from '../pages/private/Habits'
 import AppLayout from '../layouts/AppLayout'
 import AuthLayout from '../layouts/AuthLayout'
-import Login from '../pages/auth/Login'
 import PrivateRoutes from './PrivateRoutes'
-import Habit from '../pages/private/HabitDetails'
-import Calendar from '../pages/private/Calendar'
+import { Loader } from '../components/Loader'
+
+const Login = lazy(() => import('../pages/auth/Login'))
+const Dashboard = lazy(() => import('../pages/private/Dashboard'))
+const Habits = lazy(() => import('../pages/private/Habits'))
+const Calendar = lazy(() => import('../pages/private/Calendar'))
+const Habit = lazy(() => import('../pages/private/HabitDetails'))
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route element={<AuthLayout />}>
-        <Route path='/login' element={<Login />} />
-      </Route>
-
-      <Route element={<PrivateRoutes />}>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate to='/dashboard' replace />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/habits' element={<Habits />} />
-          <Route path='/habits/:id' element={<Habit />} />
-          <Route path='/calendar' element={<Calendar />} />
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path='/login' element={<Login />} />
         </Route>
-      </Route>
-    </Routes>
+
+        <Route element={<PrivateRoutes />}>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to='/dashboard' replace />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/habits' element={<Habits />} />
+            <Route path='/habits/:id' element={<Habit />} />
+            <Route path='/calendar' element={<Calendar />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }

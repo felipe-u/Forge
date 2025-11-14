20c4176341bb67d80dac8228fd444eac14349245
Felipe U <felipe_uv@outlook.com>
2025-11-13 20:54:28 -0500

    feat: Implement tsParticles/react for login background

---
+++ src/components/ParticlesBackground.tsx
@@ -0,0 +1,22 @@
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { useEffect, useState } from 'react'
+import { loadFull } from 'tsparticles'
+import { particleBgOptions } from '../utils/tsParticlesOptions'
+
+export const ParticlesBackground = () => {
+  const [init, setInit] = useState(false)
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine)
+    }).then(() => {
+      setInit(true)
+    })
+  }, [])
+
+  return (
+    <>
+      {init && <Particles id='tsparticles-ember' options={particleBgOptions} />}
+    </>
+  )
+}
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router'
 import '../styles/Auth.css'
+import { ParticlesBackground } from '../components/ParticlesBackground'
 
 export default function AuthLayout() {
   return (
     <main className='auth-main'>
       <Outlet />
+      <ParticlesBackground />
+
     </main>
   )
 }
